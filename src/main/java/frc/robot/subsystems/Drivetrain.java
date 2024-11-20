5187a14a7b2648d3520a1e7782af26e82a15bc13
Claire Qi <clairelvbunny@gmail.com>
2024-11-19 16:28:57 -0800

    committing drivetrain subsytem

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,6 +16,23 @@ public class Drivetrain extends SubsystemBase {
   private VictorSPX m_leftSecondary = new VictorSPX(0);
   /** Creates a new Drivetrain. */
   public Drivetrain() {}
+
+  public void setRightSpeed(double rightSpeed) {
+    m_rightPrimary.set(ControlMode.PercentOutput, rightSpeed);
+    m_rightSecondary.set(ControlMode.PercentOutput, rightSpeed);
+  }
+
+  public void setLeftSpeed(double leftSpeed) {
+    m_leftPrimary.set(ControlMode.PercentOutput, leftSpeed);
+    m_leftSecondary.set(ControlMode.PercentOutput, leftSpeed);
+  }
+
+  public void setSpeed(double speed) {
+    m_rightPrimary.set(ControlMode.PercentOutput, speed);
+    m_rightSecondary.set(ControlMode.PercentOutput, speed);
+    m_leftPrimary.set(ControlMode.PercentOutput, speed);
+    m_leftSecondary.set(ControlMode.PercentOutput, speed);
+  }
 
   @Override
   public void periodic() {
