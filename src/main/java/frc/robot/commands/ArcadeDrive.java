a8ce84ea8453be0078aa48ed38c563e92057075b
Claire Qi <clairelvbunny@gmail.com>
2024-11-19 16:38:20 -0800

    committing arcade drive command

---
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+
+public class ArcadeDrive extends Command {
+  private Joystick m_joystick;
+  private Drivetrain m_drivetrain;
+  /** Creates a new ArcadeDrive. */
+  public ArcadeDrive(Joystick joystick, Drivetrain drivetrain) {
+    m_joystick = joystick;
+    m_drivetrain = drivetrain;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_drivetrain.setRightSpeed(0);
+    m_drivetrain.setLeftSpeed(0);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double turn = m_joystick.getRawAxis(1)*0.7;//axis 1 is vertical axis of left stick
+    double speed = m_joystick.getRawAxis(4)*0.7;//axis 4 is vertical axis of right stick
+    
+    double left = speed + turn;
+    double right = speed - turn;
+
+    //set left and right
+    m_drivetrain.setRightSpeed(right);
+    m_drivetrain.setLeftSpeed(left);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    //set left and right to 0
+    m_drivetrain.setRightSpeed(0);
+    m_drivetrain.setLeftSpeed(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
