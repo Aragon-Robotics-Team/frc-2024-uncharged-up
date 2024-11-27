95643b8dedcd5605681b4975eb60dde90105ce2f
Claire Qi <clairelvbunny@gmail.com>
2024-11-26 16:52:09 -0800

    nothing, literally nothing

--- src/main/java/frc/robot/commands/PivotToPosition.java
+++ src/main/java/frc/robot/commands/PivotToPosition.java
@@ -6,14 +6,18 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Pivot;
-import frc.robot.subsystems.Pivot;
 
 public class PivotToPosition extends Command {
   private Pivot m_pivot;
   private double m_pivotSpeed = 0; //change later
+  private double m_goalPosition;
+  private double m_error;
+  private double m_currentPosition;
+  private double m_initialPosition;
   /** Creates a new PivotToPosition. */
-  public PivotToPosition(Pivot pivot) {
+  public PivotToPosition(Pivot pivot, double goal) {
     m_pivot = pivot;
+    m_goalPosition = goal;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_pivot);
   }
@@ -22,12 +26,15 @@ public class PivotToPosition extends Command {
   @Override
   public void initialize() {
     m_pivot.setPivotSpeed(0);
+    m_initialPosition = m_pivot.getPivotPosition();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     m_pivot.setPivotSpeed(m_pivotSpeed);
+    m_currentPosition = m_pivot.getPivotPosition() - m_initialPosition;
+    m_error = m_goalPosition - m_currentPosition;
   }
 
   // Called once the command ends or is interrupted.
@@ -39,6 +46,9 @@ public class PivotToPosition extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if(m_error < 0.1 && m_error > -0.1) {
+      return true;
+    }
     return false;
   }
 }
